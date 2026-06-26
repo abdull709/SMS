7d38fc22ba71c5c1d8b63adc96c79b2bda0ea75b
Abdullahi Abba Adam <abdullahiadam709@gmail.com>
2026-06-26 11:44:30 +0100

    db

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 const { Sequelize } = require('sequelize');
 
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const useSsl = String(process.env.DB_SSL || 'false').toLowerCase() === 'true';
